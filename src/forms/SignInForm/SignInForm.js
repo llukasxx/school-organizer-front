f1b0b9b075d6f78f58a31c49fd78116d34ee5c37
lukasx <llukasxx@gmail.com>
2016-04-22 07:33:31 -0700

    Initial auth reducer

--- src/forms/SignInForm/SignInForm.js
+++ src/forms/SignInForm/SignInForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { reduxForm } from 'redux-form'
-import { authError } from '../../redux/modules/AuthReducer'
+import * as actions from '../../redux/modules/AuthReducer'
 
 export const fields = ['email', 'password']
 
@@ -19,9 +19,12 @@ type Props = {
   handleSubmit: Function,
   fields: Object,
 };
+
 export class SignIn extends React.Component {
   props: Props;
-
+  constructor(props) {
+    super(props)
+  }
   renderErrors(field) {
     if(field.error && field.touched) {
       return (
@@ -29,12 +32,31 @@ export class SignIn extends React.Component {
       )
     }
   }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const {fields: {email, password}} = this.props
+    this.props.signInUser(email, password)
+  }
+  handleDisable = () => {
+    const { fields: {email, password} } = this.props
+    if(email.value.length > 0 && password.value.length > 0) {
+      return false
+    } else {
+      return true
+    }
+  }
+  componentDidMount() {
+    let spinner = new Spinner().spin()
+    let target = document.getElementById('spinner')
+    target.appendChild(spinner.el)
+  }
   render() {
-    const { fields: {email, password}} = this.props
+    const { fields: {email, password} } = this.props
     return (
       <div>
+        <div id="spinner" className={this.props.loading ? "" : "hidden"}></div>
         <h2 className="text-center">Welcome, please log in to continue.</h2>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="input-group input-group-lg" style={{'marginBottom': '5px'}}>
             <span className="input-group-addon" id="sizing-addon1">@</span>
             <input type="email" className="form-control" placeholder="Email" aria-describedby="sizing-addon1" {...email} />
@@ -47,7 +69,7 @@ export class SignIn extends React.Component {
           {this.renderErrors(password)}
           <hr />
           <p className="text-center">
-            <button className="btn btn-success btn-lg">Sign in</button>
+            <button disabled={this.handleDisable()} className="btn btn-success btn-lg">Sign in</button>
           </p>
         </form>
       </div>
@@ -56,13 +78,18 @@ export class SignIn extends React.Component {
 }
 
 function mapStateToProps(state) {
-  return { errorMessage: state.auth.error }
+  return { 
+    errorMessage: state.auth.error,
+    loginInfo: state.auth.loginInfo,
+    loading: state.auth.loading 
+  }
 }
+
 
 SignIn = reduxForm({
   form: 'SignIn',
   fields,
   validate
-}, mapStateToProps, authError)(SignIn)
+}, mapStateToProps, actions)(SignIn)
 
 export default SignIn
--- src/redux/modules/AuthReducer.js
+++ src/redux/modules/AuthReducer.js
@@ -1,20 +1,32 @@
 // Constants
 // export const constants = { }
-export const AUTH_ERROR = 'AUTH_ERROR'
+const START_AUTH = 'school-organizer/auth/START_AUTH'
+const AUTH_FINISH = 'school-organizer/auth/AUTH_FINISH'
+const AUTH_ERROR = 'school-organizer/auth/AUTH_ERROR'
 // Action Creators
 // export const actions = { }
-export function authError(error) {
-  return {
-    type: AUTH_ERROR,
-    payload: error
-  };
+export function signInUser({email, password}) {
+  return function(dispatch) {
+    dispatch({ type: START_AUTH })
+    return function(dispatch) {
+      dispatch({ 
+        type: AUTH_FINISH, 
+        payload: 'Login success!' 
+      })
+    }
+  }
 }
+
 // Reducer
 export const initialState = {}
 export default function (state = initialState, action) {
   switch (action.type) {
+    case START_AUTH:
+      return { ...state, loading: true };
+    case AUTH_FINISH:
+      return { ...state, loginInfo: action.payload, loading: false };
     case AUTH_ERROR:
-      return { ...state, error: action.payload };
+      return { ...state, loginInfo: action.payload, loading: false };
     default:
       return state
   }
