0bd29e7c1de1e886d281650b34002d02e55e7f3d
lukasx <llukasxx@gmail.com>
2016-06-10 06:54:11 -0700

    proper items listing

--- src/selectors/ReceiversSelector.js
+++ src/selectors/ReceiversSelector.js
@@ -20,46 +20,98 @@ const getActiveReceivers = (state) => {
   return state.receivers.activeReceivers
 }
 
+const getActiveReceiversStudentsIds = (state) => {
+  const { activeReceivers } = state.receivers
+  let studentIds = []
+  activeReceivers.map((el) => {
+    if(el.type == 'student') {
+      studentIds.push(el.id)
+    }
+  })
+  return studentIds
+}
+
+const getActiveReceiversTeachersIds = (state) => {
+  const { activeReceivers } = state.receivers
+  let teachersIds = []
+  activeReceivers.map((el) => {
+    if(el.type == 'teacher') {
+      teachersIds.push(el.id)
+    }
+  })
+  return teachersIds
+}
+
+const getActiveReceiversGroupsIds = (state) => {
+  const { activeReceivers } = state.receivers
+  let groupsIds = []
+  activeReceivers.map((el) => {
+    if(el.type == 'group') {
+      groupsIds.push(el.id)
+    }
+  })
+  return groupsIds
+}
+
+const getActiveReceiversLessonsIds = (state) => {
+  const { activeReceivers } = state.receivers
+  let lessonsIds = []
+  activeReceivers.map((el) => {
+    if(el.type == 'lesson') {
+      lessonsIds.push(el.id)
+    }
+  })
+  return lessonsIds
+}
+
 
 export const studentsArraySelector = createSelector(
-  [getAllStudents],
-  (students) => {
+  [getAllStudents, getActiveReceiversStudentsIds],
+  (students, activeReceivers) => {
     let studentsArray = []
     Object.keys(students).map((id) => {
-      studentsArray.push(students[id])
+      if(!(activeReceivers.includes(parseInt(id)))) {
+        studentsArray.push(students[id])
+      }
     })
     return studentsArray
   }
 )
 
 export const teachersArraySelector = createSelector(
-  [getAllTeachers],
-  (teachers) => {
+  [getAllTeachers, getActiveReceiversTeachersIds],
+  (teachers, activeReceivers) => {
     let teachersArray = []
     Object.keys(teachers).map((id) => {
-      teachersArray.push(teachers[id])
+      if(!(activeReceivers.includes(parseInt(id)))) {
+        teachersArray.push(teachers[id])
+      }
     })
     return teachersArray
   }
 )
 
 export const groupsArraySelector = createSelector(
-  [getAllGroups],
-  (groups) => {
+  [getAllGroups, getActiveReceiversGroupsIds],
+  (groups, activeReceivers) => {
     let groupsArray = []
     Object.keys(groups).map((id) => {
-      groupsArray.push(groups[id])
+      if(!(activeReceivers.includes(parseInt(id)))) {
+        groupsArray.push(groups[id])
+      }
     })
     return groupsArray
   }
 )
 
 export const lessonsArraySelector = createSelector(
-  [getAllLessons],
-  (lessons) => {
+  [getAllLessons, getActiveReceiversLessonsIds],
+  (lessons, activeReceivers) => {
     let lessonsArray = []
     Object.keys(lessons).map((id) => {
-      lessonsArray.push(lessons[id])
+      if(!(activeReceivers.includes(parseInt(id)))) {
+        lessonsArray.push(lessons[id])
+      }
     })
     return lessonsArray
   }
