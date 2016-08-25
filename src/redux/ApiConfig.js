01cb67de5d181392fbdd5bfe0cb5189b3f2c5355
lukasx <llukasxx@gmail.com>
2016-08-25 07:55:25 -0700

    Add heroku back-end url

--- src/redux/ApiConfig.js
+++ src/redux/ApiConfig.js
@@ -1,2 +1,6 @@
-export const ROOT_URL = 'http://localhost:3000'
+let currentUrl = 'http://localhost:3000'
+
+process.env.NODE_ENV == "production" ? currentUrl = 'https://school-organizer-back.herokuapp.com' : currentUrl = 'http://localhost:3000'
+
+export const ROOT_URL = currentUrl
 
