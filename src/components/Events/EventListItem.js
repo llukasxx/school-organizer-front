b6f75de4e758927d4cc98078ac535b4afc0ef0ae
lukasx <llukasxx@gmail.com>
2016-09-13 03:37:10 -0700

    Add event-group listing

--- src/components/Events/EventListItem.js
+++ src/components/Events/EventListItem.js
@@ -3,6 +3,19 @@ import React from 'react'
 export class EventsListItem extends React.Component {
   constructor(props) {
     super(props)
+    this.renderGroups = this.renderGroups.bind(this)
+  }
+  renderGroups() {
+    let listing = ""
+    const { groups } = this.props.event
+    if(groups.length > 0) {
+      groups.map((el) => {
+        el == groups[groups.length-1] ? listing += el.name : listing += `${el.name}, `
+      })
+      return listing
+    } else {
+      return "No groups invited"
+    }
   }
   render () {
     return (
@@ -13,7 +26,7 @@ export class EventsListItem extends React.Component {
         <br />
         <b>Created by:</b> {this.props.event.creator.name}
         <br />
-        <b>Groups:</b> TBD
+        <b>Groups:</b> {this.renderGroups()}
       </li>
     )
   }
